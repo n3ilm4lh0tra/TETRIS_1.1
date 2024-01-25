a2ca9f8aa28c5c61e81f6830de5dce4b037e9491
n3ilm4lh0tra <126697960+n3ilm4lh0tra@users.noreply.github.com>
2024-01-25 20:54:42 +0100

    Update Z_Reverse.java

--- TETRIS_1.1/src/mino/Z_Reverse.java
+++ TETRIS_1.1/src/mino/Z_Reverse.java
@@ -11,7 +11,7 @@ public class Z_Reverse extends Mino{
         b[0].y = y;
         b[1].x = b[0].x;
         b[1].y = b[0].y - Block.SIZE;
-        b[2].x = b[0].y + Block.SIZE;
+        b[2].x = b[0].x + Block.SIZE;
         b[2].y = b[0].y;
         b[3].x = b[0].x + Block.SIZE;
         b[3].y = b[0].y + Block.SIZE;
